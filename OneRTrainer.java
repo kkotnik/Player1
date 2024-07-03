24045939ce09b44644903d4021ec2e56ee80736f
kkotnik <kristjan.kotnik5@gmail.com>
2024-07-03 12:41:55 +0200

    Dodaja path

--- OneRTrainer.java
+++ OneRTrainer.java
@@ -1,9 +1,24 @@
-public static OneRModel trainOneR(List<GameData> gameDataList) {
-    String[] attributes = {"NumPlanets", "NumFleets", "PlanetColor"}; //atributi ki jih bomo uporabili
-    double bestAccuracy = 0; //kaj iscemo
-    String bestAttribute = null;
-    int bestThreshold = 0;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class OneRTrainer{
+    public static void main(String[] args) throws IOException {
+        //podatki iger
+        String folderPath = "C:\\Users\\krist\\Desktop\\2024-01-10\\Player1\\games";
 
-    return new OneRModel(bestAttribute, bestThreshold);
+        //prazen list kamer se bo shranila data
+        //GameData bom ze pol naredu
+        List<GameData> allGameDataList = new ArrayList<>();
+        
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles();
+        
+        //kam se shrani in kaj napise tocno
+        OneRModel model = trainOneR(allGameDataList);
+        String outputPath = folderPath + "\\best_attributes.txt";
+        saveModel(model, outputPath);
+        System.out.println("Best Attribute: " + model.bestAttribute + ", Best Threshold: " + model.bestThreshold);
+    }
 }
