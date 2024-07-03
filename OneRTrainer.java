8e9c864ed618a8be86bf048aa77990f4e8ece637
kkotnik <kristjan.kotnik5@gmail.com>
2024-07-04 00:10:16 +0200

    Dodani atributi

--- OneRTrainer.java
+++ OneRTrainer.java
@@ -2,8 +2,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.io.*;
 
 public class OneRTrainer{
+    private Random rand;
+
     public static void main(String[] args) throws IOException {
         //podatki iger
         String folderPath = "C:\\Users\\krist\\Desktop\\2024-01-10\\Player1\\games";
@@ -11,6 +15,7 @@ public class OneRTrainer{
         //prazen list kamer se bo shranila data
         //GameData bom ze pol naredu
         List<GameData> allGameDataList = new ArrayList<>();
+        
         
         File folder = new File(folderPath);
         File[] files = folder.listFiles();
@@ -21,4 +26,28 @@ public class OneRTrainer{
         saveModel(model, outputPath);
         System.out.println("Best Attribute: " + model.bestAttribute + ", Best Threshold: " + model.bestThreshold);
     }
+    class GameData { //vsi podatki igre, kasneje bom ekstra extends za ostale atribute
+        boolean isWinner;
+    }
+
+    class PlanetData extends GameData {
+        //drugi atributi
+            int id;
+            int x;
+            int y;
+            double size;
+            int value;
+            String color;
+        
+            PlanetData(int id, int x, int y, double size, int value, String color, boolean isWinner) {
+                this.id = id;
+                this.x = x;
+                this.y = y;
+                this.size = size;
+                this.value = value;
+                this.color = color;
+                this.isWinner = isWinner;
+            }
+        }
+        
 }
